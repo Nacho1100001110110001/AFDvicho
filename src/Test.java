b0e150252c1915294b3228e0cc2c56d177b38484
vicho1079 <vicho1079@gmail.com>
2022-04-21 21:26:47 -0400

    Se mejoro la funcionalida de la clase Test

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,8 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 public class Test {
     public static void main(String[] args){
@@ -10,7 +10,7 @@ public class Test {
         try(Scanner scanner = new Scanner(file)){
             scanner.useDelimiter(Pattern.compile(",*\\p{javaWhitespace}+"));
             AFD rut_parser = AFD.rut_test();
-            Stream<String> file_tokens = scanner.tokens();
+            List<String> file_tokens = scanner.tokens().toList();
 
             Test.true_values(file_tokens, rut_parser);
             Test.all_values(file_tokens, rut_parser);
@@ -19,15 +19,19 @@ public class Test {
             System.out.println(e.getMessage());
         }
     }
-    static void all_values(Stream<String> file_tokens, AFD afd){
+    static void all_values(List<String> file_tokens, AFD afd){
         file_tokens
+                .stream()
                 .map(x-> x +": "+ afd.process(x))
                 .forEach(System.out::println);
+        System.out.println();
     }
-    static void true_values(Stream<String> file_tokens, AFD afd){
+    static void true_values(List<String> file_tokens, AFD afd){
         file_tokens
+                .stream()
                 .filter(afd::process)
                 .map(x-> x +": true")
                 .forEach(System.out::println);
+        System.out.println();
     }
 }
