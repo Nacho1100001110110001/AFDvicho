de7901a3d3fd0f4404ee3f18b73ec1eb705e8168
vicho1079 <vicho1079@gmail.com>
2022-04-23 00:22:28 -0400

    se quitaron las partes innecesarias a la implementación.

--- src/AFD.java
+++ src/AFD.java
@@ -5,23 +5,24 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class AFD {
+    //mejora al afd eliminar el set de states ya que los estados se obtienen implicitamente
+    //del tamaño de los arreglos.
+    //cualquier int menor al tamaño del arreglo y mayor a 0 pertenese al conjunto de los state.
     private final int init_state;
     private final Set<Character> abc;
-    private final Set<Integer> states;
     private final Set<Integer> final_states;
     private final HashMap<Character, int[]> func;
 
-    public AFD(int init_state, Set<Character> abc, Set<Integer> states, Set<Integer> final_states,
+    public AFD(int init_state, Set<Character> abc, int states, Set<Integer> final_states,
                HashMap<Character, int[]> func){
-        if(init_state < 0 || abc == null || states == null || final_states == null
+        if(init_state < 0 || abc == null  || final_states == null
                 || func == null) throw new InvalidParameterException("Null parameter or invalid init state");
-        if(abc.size() == 0 || states.size() == 0 || final_states.size() == 0
+        if(abc.size() == 0 || states == 0 || final_states.size() == 0
                 || func.size() == 0) throw new InvalidParameterException("Invalid size parameter");
         if(!AFD.is_valid_func(abc, states, func)) throw new InvalidParameterException("invalid function");
 
         this.init_state = init_state;
         this.abc = new HashSet<>(abc);
-        this.states = new HashSet<>(states);
         this.final_states = new HashSet<>(final_states);
         this.func = func;
     }
@@ -48,7 +49,7 @@ public class AFD {
         return aux_process(sub_str, new_state);
     }
 
-    public static boolean is_valid_func(Set<Character> afd_abc, Set<Integer> afd_states, HashMap<Character,int[]> function){
+    public static boolean is_valid_func(Set<Character> afd_abc, int state_size, HashMap<Character,int[]> function){
         Set<Character> func_adc = new HashSet<>(function.keySet());
         Set<Character> aux_afd_abc = new HashSet<>(afd_abc);
 
@@ -59,9 +60,9 @@ public class AFD {
 
         return function.values().stream()
                     .map(xs -> Arrays.stream(xs)
-                                .mapToObj(afd_states::contains)
+                                .mapToObj(x -> x < state_size && x >= 0)
                                 .reduce(true, (b1, b2)->{
-                                    if(!(xs.length == afd_states.size())) return false;
+                                    if(!(xs.length == state_size)) return false;
                                     return b1 && b2;
                                 }))
                     .reduce(true, (b1, b2)-> b1 && b2);
@@ -76,7 +77,6 @@ public class AFD {
 
     public static AFD rut_test(){
         Set<Character> abc = AFD.setOf('-', '.','0','1', '2' ,'3', '4','5', '6','7', '8','9', 'k');
-        Set<Integer> states = AFD.setOf(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27);
         Set<Integer> final_states = AFD.setOf(10,18);
         HashMap<Character, int[]> function = new HashMap<>();
 
@@ -95,12 +95,11 @@ public class AFD {
         function.put('8', new int[]{11, 2, 3, 4, 5, 6, 7, 8,10,10,27,12,13,14,15,16,17,18,10,20,21,22,27,24,25,26,10,27});
         function.put('9', new int[]{11, 2, 3, 4, 5, 6, 7, 8,10,10,27,12,13,14,15,16,17,18,10,20,21,22,27,24,25,26,10,27});
 
-        return new AFD(0, abc, states, final_states, function);
+        return new AFD(0, abc, 28, final_states, function);
     }
 
     public static AFD nx3_test(){
         Set<Character> abc = AFD.setOf('a', 'b');
-        Set<Integer> states = AFD.setOf(0,1,2,3);
         Set<Integer> final_states = AFD.setOf(3);
 
         HashMap<Character, int[]> function =new HashMap<>();
@@ -109,7 +108,7 @@ public class AFD {
         function.put('a', new int[]{ 1, 2, 3, 1});
         function.put('b', new int[]{ 1, 2, 3, 1});
 
-        return new AFD(0, abc, states, final_states, function);
+        return new AFD(0, abc, 4, final_states, function);
 
     }
 }
